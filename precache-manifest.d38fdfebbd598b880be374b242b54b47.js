57a3f81adbe0da11216dcae65192dd7212f71c2d
Ahmed Farghal <a.fegoo2@gmail.com>
2018-11-07 03:27:12 +0200

    Updates

---
+++ precache-manifest.d38fdfebbd598b880be374b242b54b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1160b6d78a551dc5708b",
+    "url": "/static/js/main.1160b6d7.chunk.js"
+  },
+  {
+    "revision": "f6ac94d4b08e0c891787",
+    "url": "/static/js/1.f6ac94d4.chunk.js"
+  },
+  {
+    "revision": "1160b6d78a551dc5708b",
+    "url": "/static/css/main.96a48c55.chunk.css"
+  },
+  {
+    "revision": "1cf52c03759a760e7776e4488873d9e8",
+    "url": "/index.html"
+  }
+];
